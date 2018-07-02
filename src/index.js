f5babc6cb8d2f3a5e010c7805d800019eaa08666
Liam O'Sullivan <lm_osullivan@yahoo.co.uk>
2018-07-02 10:33:53 +0100

    Add winner detection

--- src/index.js
+++ src/index.js
@@ -48,6 +48,9 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares)|| squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X':'O';
     this.setState({
       squares: squares,
@@ -57,6 +60,7 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
+
     return (
     <Square
       value={this.state.squares[i]}
@@ -68,8 +72,16 @@ class Board extends React.Component {
   }
 
   render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner){
+      status = 'Winner'+winner;
+
+    }
+    else{
+      status = 'Next Player'+(this.state.xIsNext ? 'X' : 'O');
+    }
     // console.log(this.state.squares);
-    const status = 'Next player: X';
 
     return (
       <div>
@@ -110,6 +122,26 @@ class Game extends React.Component {
   }
 }
 
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 ReactDOM.render(
